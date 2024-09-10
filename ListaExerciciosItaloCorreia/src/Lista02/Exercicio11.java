74c65b6b6470bb2bb5a01e53f10934547e91bd09
Italo Correia <128881501+italocaraujo@users.noreply.github.com>
2024-09-10 09:16:51 -0300

    Add files via upload

---
+++ ListaExerciciosItaloCorreia/src/Lista02/Exercicio11.java
@@ -0,0 +1,36 @@
+package Lista02;
+
+import java.util.Scanner;
+
+public class Exercicio11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Variáveis
+        int a, b, trocaValores;
+
+        // Solicitação e leitura de dados
+        System.out.println("Digite o valor de \"a\": ");
+        a = input.nextInt();
+
+        System.out.println("Digite o valor de \"b\": ");
+        b = input.nextInt();
+
+        // Trocando os valores de "a" e "b"
+
+        trocaValores = b;
+        b = a;
+        a = trocaValores;
+
+        /*
+        a variável "trocaValores" vai receber o valor de "b"
+        "b" vai receber o valor de "a"
+        "a" vai receber o valor da variável "trocaValores"
+        */
+
+        // Resultado
+        System.out.println("O valor de \"a\" agora é " + a);
+        System.out.println("o valor de \"b\" agora é " + b);
+
+    }
+}
